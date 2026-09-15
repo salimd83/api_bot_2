c3f54ff067c3c18aac28f35ae43f39301860286f
Salim Dirani <salimdirani@gmail.com>
2026-09-15 07:34:39 +0300

    adding DB_PATH to .env

--- rest_api.py
+++ rest_api.py
@@ -1,10 +1,15 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 
-DB_PATH = Path(__file__).resolve().parent / "live_bot_2.db"
+# Load the variables from the .env file
+load_dotenv()
+
+DB_PATH = Path(__file__).resolve().parent / os.getenv("DB_PATH", "trading.db")
 
 app = FastAPI()
 
